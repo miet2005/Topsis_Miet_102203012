4781a741ad7a7982f7d716853786ca8b4c519f28
miet2005 <mietpamecha10@gmail.com>
2025-01-26 01:37:06 +0530

    second commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import pathlib
 from setuptools import setup
 
 # The directory containing this file
-HERE = pathlib.Path(_file_).parent
+HERE = pathlib.Path(__file__).parent
 
 # The text of the README file
 README = (HERE / "README.md").read_text()
@@ -14,9 +14,9 @@ setup(
     description="Multiple Criteria Decision Making using Topsis",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/uditvashisht/saral-square",
-    author="Udit Vashisht",
-    author_email="admin@saralgyaan.com",
+    url="https://github.com/miet2005/Topsis_Miet_102203012",
+    author="Miet Pamecha" ,
+    author_email="mietpamecha10@gmail.com",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
@@ -24,12 +24,12 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    packages=["square"],
+    packages=["topsis"],
     include_package_data=True,
     install_requires=[],
     entry_points={
         "console_scripts": [
-            "square=square._main_:main",
+            "topsis=topsis._main_:main",
         ]
     },
 )
